d090159460d0fa1c9202e1d333d12ee1bbf83f99
Rashad Awad <rashadawad@Rashads-MacBook-Pro.local>
2024-01-24 23:05:39 +0200

    some commit comment

--- lib/components/add_note_button_sheet.dart
+++ lib/components/add_note_button_sheet.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/components/custom_text_field.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
   const AddNoteButtonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: const Column(
+        children: [
+          SizedBox(
+            height: 25,
+          ),
+          CustomTextField(),
+        ],
+      ),
     );
   }
 }
