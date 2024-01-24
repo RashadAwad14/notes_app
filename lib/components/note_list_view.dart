269e66a1622a3704d6154b5d2411dbd27f1b78e2
Rashad Awad <rashadawad@192.168.1.10>
2024-01-24 04:20:51 +0200

    some commit comment

--- lib/components/note_list_view.dart
+++ lib/components/note_list_view.dart
@@ -11,7 +11,7 @@ class NotesListView extends StatelessWidget {
       child: ListView.builder(
         itemBuilder: (context, index) {
           return const Padding(
-            padding: EdgeInsets.symmetric(vertical:8.0),
+            padding: EdgeInsets.symmetric(vertical:5.0),
             child: NoteItem(),
           );
         },
