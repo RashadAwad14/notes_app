971dbabb45be0c12830ab1a805fc99bb27deb33f
Rashad Awad <rashadeslmanrashad@gmail.com>
2024-01-25 03:29:08 +0200

    add addNote methood to add note cubit

--- lib/cubits/add_note_cubit.dart
+++ lib/cubits/add_note_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
@@ -10,8 +12,16 @@ class AddNoteCubit extends Cubit<AddNoteState> {
 
 
 
-  addNote(NoteModel note)
-  {
-    
+  addNote(NoteModel note) async {
+
+    emit(AddNoteLoading());
+    try {
+  var notesBox =     Hive.box<NoteModel>(kNotesBox);
+
+  emit(AddNoteSuccess());
+  await notesBox.add(note);
+}  catch (e) {
+  AddNoteFailure(errorMessage: e.toString());
+}
   }
 }
