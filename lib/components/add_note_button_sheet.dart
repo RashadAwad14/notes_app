d5574750ef5d6d5d719be33dcc5ec08088c56438
Rashad Awad <rashadeslmanrashad@gmail.com>
2024-01-25 04:04:10 +0200

    provide add note cubit

--- lib/components/add_note_button_sheet.dart
+++ lib/components/add_note_button_sheet.dart
@@ -1,87 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/components/custom_button.dart';
-import 'package:notes_app/components/custom_text_field.dart';
+import 'package:notes_app/cubits/add_note_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'add_note_bottom_sheet.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
   const AddNoteButtonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return  Padding(
       padding: EdgeInsets.symmetric(horizontal: 12),
       child: SingleChildScrollView(
-        child: AddNoteForm(),
+        child: BlocConsumer<AddNoteCubit, AddNoteState>(
+          listener: (context, state) {
+            // TODO: implement listener
+          },
+          builder: (context, state) {
+            return AddNoteForm();
+          },
+        ),
       ),
     );
   }
 }
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-
-  final GlobalKey<FormState> formKey = GlobalKey();
-
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-
-  String? title, subTitle;
-  @override
-  Widget build(BuildContext context) {
-    return  Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child:  Column(
-        children: [
-          const SizedBox(
-            height: 25,
-          ),
-          CustomTextField(
-            onSaved: (value) {
-              title = value;
-            },
-            hint: 'Title',
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            onSaved: (value) {
-              subTitle = value;
-            },
-            hint: 'Content',
-            maxLines: 5,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          CustomButton(
-            onTap: () {
-              if(formKey.currentState!.validate())
-              {
-                formKey.currentState!.save();
-              } else
-              {
-                autovalidateMode = AutovalidateMode.always;
-                setState(() {
-                  
-                });
-              }
-            },
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
