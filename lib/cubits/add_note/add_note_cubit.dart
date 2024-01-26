991648664d48e184a6fec64e7ed704cc03a09665
Rashad Awad <rashadeslmanrashad@gmail.com>
2024-01-26 03:05:32 +0200

    add add note bottom sheet colors done!

--- lib/cubits/add_note/add_note_cubit.dart
+++ lib/cubits/add_note/add_note_cubit.dart
@@ -10,18 +10,17 @@ class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
 
-
-
+Color color = const Color(0xffAC3931);
   addNote(NoteModel note) async {
-
+    note.color = color.value;
     emit(AddNoteLoading());
     try {
-  var notesBox =     Hive.box<NoteModel>(kNotesBox);
-
-  await notesBox.add(note);
-  emit(AddNoteSuccess());
-}  catch (e) {
-  emit(AddNoteFailure(errorMessage: e.toString()));
-}
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      await notesBox.add(note);
+      emit(AddNoteSuccess());
+    } catch (e) {
+      emit(AddNoteFailure(e.toString()));
+    }
   }
 }
+
--- lib/cubits/add_note/add_note_state.dart
+++ lib/cubits/add_note/add_note_state.dart
@@ -9,7 +9,7 @@ class AddNoteSuccess extends AddNoteState {}
 class AddNoteFailure extends AddNoteState {
   final String errorMessage;
 
-  AddNoteFailure({required this.errorMessage});
+  AddNoteFailure(this.errorMessage);
 }
 
 
